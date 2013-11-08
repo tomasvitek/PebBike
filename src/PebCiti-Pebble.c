b64bc3f06f4e8daf3f583aaf6734af4ebd5abb95
Joe Masilotti <joseph.masilotti@gmail.com>
2013-11-08 13:29:40 -0500

    Display station name and focus; add vibrations.

--- src/PebCiti-Pebble.c
+++ src/PebCiti-Pebble.c
@@ -9,18 +9,36 @@ PBL_APP_INFO(MY_UUID, "PebCiti", "Masilotti.com", 1, 0, DEFAULT_MENU_ICON, APP_I
 static struct PebCitiData
 {
     Window window;
-    TextLayer text_layer;
+    TextLayer focus_layer;
+    TextLayer station_layer;
+    uint8_t vibrate;
     AppSync sync;
-    uint8_t sync_buffer[32];
+    uint8_t sync_buffer[124];
 } s_data;
 
 enum {
-    PEB_CITI_TEXT_KEY = 0x1
+    PEB_CITI_FOCUS_KEY = 0x0,
+    PEB_CITI_STATION_KEY = 0x1,
+    PEB_CITI_VIBRATE_KEY = 0x2
 };
 
 static void sync_tuple_changed_callback(const uint32_t key, const Tuple *new_tuple, const Tuple *old_tuple, void *context)
 {
-    text_layer_set_text(&s_data.text_layer, new_tuple->value->cstring);
+    switch (key) {
+        case PEB_CITI_FOCUS_KEY:
+            text_layer_set_text(&s_data.focus_layer, new_tuple->value->cstring);
+            break;
+        case PEB_CITI_STATION_KEY:
+            text_layer_set_text(&s_data.station_layer, new_tuple->value->cstring);
+            break;
+        case PEB_CITI_VIBRATE_KEY:
+            s_data.vibrate = new_tuple->value->uint8;
+            break;
+    }
+
+    if (s_data.vibrate == 1) {
+        vibes_short_pulse();
+    }
 }
 
 static void peb_citi_init(AppContextRef context)
@@ -28,12 +46,18 @@ static void peb_citi_init(AppContextRef context)
     Window *window = &s_data.window;
     window_init(window, "PebCiti");
 
-    text_layer_init(&s_data.text_layer, GRect(0, 100, 144, 68));
-    layer_add_child(&window->layer, &s_data.text_layer.layer);
-    text_layer_set_text_alignment(&s_data.text_layer, GTextAlignmentCenter);
+    text_layer_init(&s_data.focus_layer, GRect(5, 10, 134, 25));
+    text_layer_set_text_alignment(&s_data.focus_layer, GTextAlignmentCenter);
+    layer_add_child(&window->layer, &s_data.focus_layer.layer);
+
+    text_layer_init(&s_data.station_layer, GRect(5, 30, 134, 65));
+    text_layer_set_text_alignment(&s_data.station_layer, GTextAlignmentCenter);
+    layer_add_child(&window->layer, &s_data.station_layer.layer);
 
     Tuplet initial_values[] = {
-        TupletCString(PEB_CITI_TEXT_KEY, "<empty>")
+        TupletCString(PEB_CITI_FOCUS_KEY, "Closest Available Bike:"),
+        TupletCString(PEB_CITI_STATION_KEY, "Wating for iPhone app..."),
+        TupletInteger(PEB_CITI_VIBRATE_KEY, (uint8_t) 0)
     };
 
     app_sync_init(&s_data.sync,
@@ -60,7 +84,7 @@ void pbl_main(void *params)
         .deinit_handler = &peb_citi_deinit,
         .messaging_info = {
             .buffer_sizes = {
-                .inbound = 64,
+                .inbound = 128,
                 .outbound = 16
             }
         }
